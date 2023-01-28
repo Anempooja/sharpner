d9c9153ae6e8018aed21d88514e24c7fc1288e62
Anempooja <anempooja@email.com>
2023-01-28 20:26:03 +0530

    database

--- ExpenseAppUtil/database.js
+++ ExpenseAppUtil/database.js
@@ -1,5 +1,7 @@
-
+const dotenv=require('dotenv')
+dotenv.config()
 const Sequilize=require('sequelize')
+console.log(process.env.DB_NAME,process.env.DB_PASSWORD,process.env.DB_HOST,process.env.DB_USERNAME)
 const sequilize=new Sequilize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
     dialect:'mysql',
     host:process.env.DB_HOST

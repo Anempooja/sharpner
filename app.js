1ff37c1b1fed21450d95e3ac1a329e69398abb0b
Anempooja <anempooja@email.com>
2023-02-10 20:42:47 +0530

    made app to scale

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv=require('dotenv')
+var CronJob = require('cron').CronJob;
 var sequelize=require('./chatappUtil/database')
 const bodyParser = require('body-parser');
 
@@ -8,10 +8,12 @@ const Group=require('./chatappModels/group')
 const User=require('./chatappModels/user')
 const Chat=require('./chatappModels/chat')
 const userGroup=require('./chatappModels/userGroup') 
+//const ArchievedChat=require('../chatappModels/archievedChats')
 
 var signupRouter=require('./chatappRoutes/signup')
 var chatRouter=require('./chatappRoutes/chat')
 var groupRouter=require('./chatappRoutes/group');
+const ArchievedChat = require('./chatappModels/archievedChats');
 
 var app=express()
 
@@ -26,8 +28,30 @@ Chat.belongsTo(User)
 Group.hasMany(Chat)
 Chat.belongsTo(Group)
 
+User.hasMany(ArchievedChat)
+ArchievedChat.belongsTo(User)
+
+Group.hasMany(ArchievedChat)
+ArchievedChat.belongsTo(Group)
+
 Group.belongsToMany(User,{through:userGroup})
 User.belongsToMany(Group,{through:userGroup})
+
+var job = new CronJob(
+	'* * 3 * * *',
+	async function() {
+		// console.log('You will see this message every second');
+        const chats=await Chat.findAll()
+        chats.forEach(chat => {
+            ArchievedChat.create({message:chat.message,userId:chat.userId,name:chat.name,groupId:chat.groupId,createdAt:chat.createdAt,updatedAt:chat.updatedAt})
+            Chat.destroy({where:{id:chat.id}})
+        });
+        
+	},
+	null,
+	true,
+	'Asia/Kolkata'
+);
 sequelize.sync()
 .then(()=>{
     app.listen(3000)

4ffd48c7bd541a890262d0893935d07564f5cf2e
Anempooja <anempooja@email.com>
2023-02-21 13:32:24 +0530

    oders

--- models/user.js
+++ models/user.js
@@ -85,6 +85,30 @@ class User{
     })
     .catch(err=>{console.log(err)})
   }
+  addOrder(){
+    const db=getDb()
+    return this.getCart()
+    .then(products=>{
+      const order={
+        items:products,
+        user:{
+          _id:new ObjectId(this._id),
+          name:this.name
+  
+        }
+      }
+      return db.collection('orders').insertOne(order)
+    })
+    .then(result=>{
+      this.cart= {items:[]}
+      return db.collection('users')
+      .updateOne(
+        {_id:new ObjectId(this._id)},
+        {$set:{cart:{items:[]}}}
+      )
+
+    })
+  }
 }
 
 

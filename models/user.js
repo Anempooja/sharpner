4ce7e6e0d166a85a96ee794c91d049d8b6f40368
Anempooja <anempooja@email.com>
2023-02-21 12:39:21 +0530

    delete  in cart

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongodb=require('mongodb');
+const { UCS2_DANISH_CI } = require('mysql2/lib/constants/charsets');
 const { use, get } = require('../routes/admin');
 
 const getDb=require('../util/database').getDb
@@ -15,6 +16,7 @@ class User{
    return db.collection('users').insertOne(this)
   }
   addToCart(product) {
+    
     const cartProductIndex = this.cart.items.findIndex(cp => {
       return cp.productId.toString() === product._id.toString();
     });
@@ -39,6 +41,39 @@ class User{
       .updateOne(
         { _id: new ObjectId(this._id) },
         { $set: { cart: updatedCart } }
+      );
+  }
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p,
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: {items: updatedCartItems} } }
       );
   }
   static findById(userId){
